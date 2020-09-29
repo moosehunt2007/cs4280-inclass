c18ebaba164f0cff1ce2ee7b12ae2d6519d850d7
William Hunt <williamhunt1@mail.weber.edu>
2020-09-29 16:49:06 -0600

    After 9/28

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -9,10 +9,11 @@ import vs_script from "../shaders/vertex-color.glsl"
 import fs_script from "../shaders/fragment-color.glsl"
 
 // JavaScript
-//  import { scribble, displayMultiprogram } from './week05'
+ import { scribble, displayMultiprogram, sierpinski } from './week05'
 //  displayMultiprogram()
+sierpinski()
 
 // import { displayColoredTriangles } from './Activity3'
 
 import {displayCube} from './week06'
-displayCube()
+// displayCube()
